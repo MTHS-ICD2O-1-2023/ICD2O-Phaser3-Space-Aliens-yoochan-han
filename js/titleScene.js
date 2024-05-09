f295971727faf32d51c6c6d2e40723d76a36af81
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-09 18:14:36 +0000

    #

--- js/titleScene.js
+++ js/titleScene.js
@@ -41,7 +41,7 @@ class TitleScene extends Phaser.Scene {
    */
   preload() {
     console.log("Title Scene")
-    this.load.image("titleSceneBackgorund", "./assets/aliens_screen_image.jpg")
+    this.load.image("titleSceneBackground", "./assets/aliens_screen_image.jpg")
   }
   /**
  * Can be defined on your own Scenes.
