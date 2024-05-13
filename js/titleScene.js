8b06992581309c2f219fe71b4c74dc0c848ab3da
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-13 17:59:56 +0000

    #

--- js/titleScene.js
+++ js/titleScene.js
@@ -66,7 +66,9 @@ create(data) {
    *    @param {number} delta - the delta time in MS since the last frame
    */
   update(time, delta) {
-    //pass
+    if (time > 3000) {
+      this.scene.switch("menuScene")
+    }
   }
 }
 
