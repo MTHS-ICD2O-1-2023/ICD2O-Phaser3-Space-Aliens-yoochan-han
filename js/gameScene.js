ca1ce59102e4526ad4aa948d37756b1807635bf3
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-13 20:34:45 +0000

    msl lock, Fox2!!

--- js/gameScene.js
+++ js/gameScene.js
@@ -40,6 +40,7 @@ preload() {
 
   this.load.image("starBackground", "./assets/starBackground.png")
   this.load.image("ship", "./assets/spaceShip.png")
+  this.load.image("missile", "./assets/missile.png")
 }
 
   /**
@@ -52,6 +53,10 @@ preload() {
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+
+    //create a group for the missiles
+    this.missileGroup = this.physics.add.group()
   }
 
   /**
@@ -63,6 +68,7 @@ preload() {
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
@@ -76,7 +82,19 @@ preload() {
         this.ship.x = 1920
       }
     }
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        //fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        this.missileGroup.add(aNewMissile)
+        }
+      }
+
+      if (keySpaceObj.isUp === true) {
+        this.fireMissile = false
+      }
+    }
   }
-}
 
 export default GameScene
